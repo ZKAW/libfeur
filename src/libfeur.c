af699938aefe47661bc1d77882b887042241d461
ZKAW <zkaw.exod@gmail.com>
2024-03-25 16:54:13 +0100

    feat: remove \n

--- src/libfeur.c
+++ src/libfeur.c
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define NATIVE_MALLOC 0
+#define NATIVE_MALLOC 1
 
 void* malloc(size_t size) {
-    write(2, "FEUR\n", 5);
+    write(2, "FEUR", 5);
     void* (*libc_malloc)(size_t) = dlsym(RTLD_NEXT, "malloc");
     if (NATIVE_MALLOC)
         return libc_malloc(size);
